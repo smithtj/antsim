4dea7fb77a545fd4701ac176257b4822540430b2
smithtj <tjsmithdev@gmail.com>
2019-08-15 00:59:38 +0100

    Added logging and updated properties reader.

--- src/main/java/com/worldwarant/antsim/Program.java
+++ src/main/java/com/worldwarant/antsim/Program.java
@@ -1,7 +1,42 @@
 package com.worldwarant.antsim;
 
+import com.worldwarant.antsim.Simulation.Simulator;
+import com.worldwarant.antsim.common.GameUtils;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Program {
+
+    private final static Logger logger = LogManager.getLogger(Program.class);
+
     public static void main(String[] args) {
+        runSimulation();
+    }
+
+    public static void runSimulation() {
+        runSimulation("gameConfig.properties");
+    }
+
+    public static void runSimulation(String gameConfig) {
+        try {
+            logger.log(Level.DEBUG, String.format("Trying to run simulation with properties from %S.", gameConfig));
+            Configuration gameConfiguration = GameUtils.loadProperties(gameConfig);
+            logger.log(Level.DEBUG, "Game configuration loaded.");
+            logger.log(Level.DEBUG, "Creating simulator");
+            Simulator antsim = new Simulator(gameConfiguration);
+            logger.log(Level.DEBUG, "Starting to run simulation");
+            antsim.runSimulation();
+        } catch (ConfigurationException e) {
+            logger.log(Level.ERROR, "Configuration failed.");
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            logger.log(Level.ERROR, "Simulation failed!");
+            logger.log(Level.ERROR, "The ants died of dysentery.");
+            logger.log(Level.ERROR, e.getMessage());
+        }
 
     }
 }
